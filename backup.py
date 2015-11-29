c16d78916f2bc727f53fc160a817d803632b17e1
Daniel Amick <dka271@cedar.rlogin>
2015-11-29 07:30:19 -0500

    Completed with comments and error checking

--- backup.py
+++ backup.py
@@ -10,44 +10,58 @@ import sys
 import tarfile
 import datetime
 
+#Gets the current time
 currentTime= str( datetime.datetime.now() )
 
+#Get the current path
 path = os.getcwd()
 
+#Checks to make sure that there is an argument
 if (len(sys.argv) != 2):
 	print("You must enter a directory")
 	exit()
 
+#Checks to make sure that the directory exists
+if (os.path.isdir(path)):
+	print("The directory entered does not exist")
+	exit()
+
+#Sets the sender and recipient of the email
 sender = "dka271@vt.edu"
 recipient = "dka271@vt.edu"
 
+#Sets the subject and applies To and From to the email
 message = MIMEMultipart()
-
 message[ 'From' ] = sender
 message[ 'To' ] = recipient
 message[ 'Subject' ] = "This is a back-up of "+ sys.argv[1] + " on " + currentTime
 
+#Sets and attaches the body text for the email
 textBody = "Directory passed in: " +  sys.argv[1] + "\nTime it was backed up: " + currentTime
 message.attach(MIMEText(textBody, 'plain'))
 
+#Recursively creates the tar file
 with tarfile.open("tarfile.tar.gz", mode='w:gz') as archive:
 	archive.add(sys.argv[1], recursive=True)
 
+#Creates the tar file as an attachment
 fileName = "tarfile.tar.gz"
 path = path + "/" + fileName
 attachment = open(path, "rb")
 
+#Convert the file into base 64 for sending
 part = MIMEBase('application', 'octet-stream')
 part.set_payload((attachment).read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= %s" % fileName)
 
+#Attach the file
 message.attach(part)
 
+#Actually connects securely to the server, and sends the email
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(sender, "Medaridley-1")
 text = message.as_string()
 server.sendmail(sender, recipient, text)
 server.quit()
-
